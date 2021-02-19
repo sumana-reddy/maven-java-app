afc45537543aeb28825f0d51b2b95f283409d6fa
sumanareddy <s538360@nwmissouri.edu>
2021-02-19 15:09:17 -0500

    Add initial Maven Project

---
+++ src/main/java/edu/nwmissouri/bigdata/superstar/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdata.superstar;
+
+/**
+ * This begins execution of my app.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from Sumana" );
+    }
+}
